bccf9bf8cc1ad093308c9ceb86a33c63b25ca4a3
clinicon2027 <clinicon2027@gmail.com>
2026-09-21 12:20:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Clinicon2027/',
+    
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
